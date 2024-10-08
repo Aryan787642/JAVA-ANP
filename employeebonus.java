f96bd94ab99640e0fe524a0ebb01139214fd19f6
Aryan787642 <151537807+Aryan787642@users.noreply.github.com>
2024-10-08 18:39:18 +0530

    Update employeebonus.java

--- employeebonus.java
+++ employeebonus.java
@@ -1,7 +1,8 @@
 import java.util.Scanner;
 
 public class Bonus {
-    public static void main(String[] args) {
+    public static void main(String[] args) 
+    {
         Scanner sn = new Scanner(System.in);
 
       
