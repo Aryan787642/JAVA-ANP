ec7bf72a347b0a898b540a9b14fa25cf9e4cdaad
Aryan787642 <151537807+Aryan787642@users.noreply.github.com>
2024-10-08 17:04:40 +0530

    Update votelegibility.java

--- votelegibility.java
+++ votelegibility.java
@@ -1,7 +1,7 @@
 import java.util.Scanner;
 
 public class votelegibility {
-    public static void main(String[] args) {
+    public static void main(String args[]) {
         Scanner sn = new Scanner(System.in);
         
         System.out.print("Enter age: ");
